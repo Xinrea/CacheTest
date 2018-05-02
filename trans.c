84bfd7e5082fd98ea5887584e1aa46f6211c58e9
Xinrea <s11s@vip.qq.com>
2018-05-02 22:48:35 +0800

    modified:   trans.c

--- trans.c
+++ trans.c
@@ -22,51 +22,76 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
-		int r1,r2;
+		int r1,r2,r3,r4,r5,r6,r7,r8;
 		switch(M){
 				case 32:{
-								for(int i = 0; i < M; i+=8){
-										for(int j = 0; j < N; j+=8){
-												for(int k = j; k < j+8&& k < N; ++k){
-														for(int l = i; l < i+8&& l < M; ++l){
-																if(k!=l)B[l][k] = A[k][l];
-																else {
-																		r1 = k;
-																		r2 = A[k][k];
-																}	
-														}
-														if(i==j){
-																B[r1][r1] = r2;
-														}
-												}
+							for(int i = 0; i < M; i+=8){
+								for(int j = 0; j < N; j+=8){
+									for(int k = j; k < j+8&& k < N; ++k){
+										for(int l = i; l < i+8&& l < M; ++l){
+											if(k!=l)B[l][k] = A[k][l];
+											else {
+												r1 = k;
+												r2 = A[k][k];
+											}	
 										}
+										if(i==j){
+											B[r1][r1] = r2;
+										}
+									}
 								}
+							}
 								break;
 						}
 				case 64:{
-								for(int i = 0; i < N; i+=8){
-										for(int j = 0; j < M; j+=8){
-												for(int k = i; k < i+8; ++k){
-														for(int l = j; l < j+8; ++l){
-															B[l][k] = A[k][l];
-														}
-												}
-										
+							for(int i = 0; i < N; i+=8){
+								for(int j = 0; j < M; j+=8){
+									for(int k = 0; k < 8; ++k){
+										r1 = A[i+k][j];
+										r2 = A[i+k][j+1];
+										r3 = A[i+k][j+2];
+										r4 = A[i+k][j+3];
+										if(!k){
+											r5 = A[i+k][j+4];
+											r6 = A[i+k][j+5];
+											r7 = A[i+k][j+6];
+											r8 = A[i+k][j+7];
 										}
+										B[j][i+k] = r1;
+										B[j+1][i+k] = r2;
+										B[j+2][i+k] = r3;
+										B[j+3][i+k] = r4;
+									}
+									for(int k = 7; k > 0; --k){
+										r1 = A[i+k][j+4];
+										r2 = A[i+k][j+5];
+										r3 = A[i+k][j+6];
+										r4 = A[i+k][j+7];
+										B[j+4][i+k] = r1;
+										B[j+5][i+k] = r2;
+										B[j+6][i+k] = r3;
+										B[j+7][i+k] = r4;
+									}
+									B[j+4][i] = r5;
+									B[j+5][i] = r6;
+									B[j+6][i] = r7;
+									B[j+7][i] = r8;
+
 								}
+							}
 								break;
 						}
 				case 61:{
-								for(int i = 0; i < M; i+=16){
-										for(int j = 0; j < N; j+=4){
-												for(int k = j; k < j+4&& k < N; ++k){
-														for(int l = i; l < i+16&& l < M; ++l){
-																B[l][k] = A[k][l];	
-														}
-												}
+							for(int i = 0; i < M; i+=16){
+								for(int j = 0; j < N; j+=4){
+									for(int k = j; k < j+4&& k < N; ++k){
+										for(int l = i; l < i+16&& l < M; ++l){
+											B[l][k] = A[k][l];	
 										}
+									}
 								}
-								break;
+							}
+							break;
 						}
 		}
 		
